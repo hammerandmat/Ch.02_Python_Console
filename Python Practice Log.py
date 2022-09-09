8c4b08863ef285e31e8e85facb17364dfc7b8f9a
hammerandmat <hammerandmat@urbandaleschools.com>
2022-09-09 08:59:38 -0500

    python practice

---
+++ Python Practice Log.py
@@ -0,0 +1,34 @@
+# This is my learning about Python
+# This is another line
+print(10.1//2)  #floor division
+print(7%2)  #modulus get the remainder
+print(abs(-7))  #absolute value
+a=2
+b=7
+print(a*b)
+print(b**a)
+print() #blank line
+print(b%2)
+
+a,b=(5,4)
+print(a)
+print(b)
+
+x=8
+print(x)
+x=x+1   #same as x+=1
+x=x*2   #same as x*=2
+print(x)
+bulletsShot=0
+bulletsShot+=3
+print(bulletsShot)
+
+a = "this is some text"
+b=3
+c=4.3
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
